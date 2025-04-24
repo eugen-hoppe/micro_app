b919aa23c9b3d287e8db801be6f70cbf08c58b18
Eugen <eugen.hoppe.de@gmail.com>
2025-04-24 11:47:19 +0200

    generic approach start

--- dev.py
+++ dev.py
@@ -7,5 +7,4 @@ print(f"export PYTHONPATH=$PYTHONPATH:{os.getcwd()}")
 if __name__ == "__main__":
     import uvicorn
 
-    # uvicorn.run("app:api", host="localhost", port=8000, reload=True)
-  
+    uvicorn.run("src.application:app", host="localhost", port=8000, reload=True)
--- src/application.py
+++ src/application.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from src.db.models import Base
-
 
 
 app = FastAPI(
@@ -19,3 +18,14 @@ engine = create_engine(url=sql_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next):
+    response = Response("Internal server error", status_code=500)
+    try:
+        setattr(request.state, "db", SessionLocal())
+        response = await call_next(request)
+    finally:
+        session: Session = getattr(request.state, "db")
+        session.close()
+    return response
